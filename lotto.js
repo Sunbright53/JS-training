f9f32400bf34af09d95d65c2ce5c9f60b470a653
Sunbright53 <141810435+Sunbright53@users.noreply.github.com>
2024-05-16 21:46:30 +0700

    feat:create file learing JS

---
+++ lotto.js
@@ -0,0 +1,3 @@
+let number = prompt("เลขที่ต้องการซื้อ :");
+document.getElementById("result").innerHTML = Math.floor(Math.random() * 100);
+// random
---
+++ webfor18.js
@@ -0,0 +1,4 @@
+let age = prompt("กรุณากรอกอายุของผู้ใฃ้งาน");
+if (age > 18) {
+  document.getElementById("content").innerHTML = "เดี๋ยวจะเจอของจริง";
+}
